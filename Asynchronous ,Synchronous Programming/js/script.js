befd58ea745c688bfa06134baf8b363c5a767904
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-02-02 01:23:38 +0500

    Add files via upload

---
+++ Asynchronous ,Synchronous Programming/js/script.js
@@ -0,0 +1,18 @@
+
+/*
+let a = prompt("What is your name?")
+let b = prompt("What is your age?")
+let c = prompt("What is your Favorite color?")
+console.log("My name is " + a + " I am " + b + " year old " + c + " ais my favorite color")
+*/
+
+// Asynchronous Programming;
+console.log('start')
+setTimeout(function(){
+    console.log("Hey I am Good")
+    console.log("End")
+},3000)
+
+
+// CallBacks;
+
